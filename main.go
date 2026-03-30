42db18cc08c84f22331ff99188872397373b3f0a
Ayush Patel <ayush005rkt@gmail.com>
2026-03-31 03:35:06 +0530

    error handler

--- main.go
+++ main.go
@@ -28,8 +28,8 @@ func main() {
 		MaxAge:           300,
 	}))
     v1Router:=chi.NewRouter()
-	v1Router.HandleFunc("/health",handlerReadiness)
-	v1Router.Mount("/v1",v1Router)
+	v1Router.Get("/health",handlerReadiness)
+	router.Mount("/v1",v1Router)
 	srv := &http.Server{
 		Handler: router,
 		Addr:    ":" + portString,
